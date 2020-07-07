b383737580434af0e0e44bf528b6e51ef7197a21
Dasc1227 <davidsolco@gmail.com>
2020-07-07 14:11:44 -0600

    [Modulo Refactoring]
    [Cambio de ifs a Switch en get masa y tamano precio]

--- Examen2_Solorzano_David/Examen2_Solorzano_David/Modelos/PizzaModel.cs
+++ Examen2_Solorzano_David/Examen2_Solorzano_David/Modelos/PizzaModel.cs
@@ -20,27 +20,44 @@ namespace Examen2_Solorzano_David.Modelos
         public int getMasaPrice(string masa)
         {
             int precio = 0;
-            if (masa.Equals("original"))
-                precio += 1000;
-            if (masa.Equals("crunchy"))
-                precio += 1500;
-            if (masa.Equals("sarten") || masa.Equals("queso"))
-                precio += 2000;
+            switch (masa)
+            {
+                case "original":
+                    precio += 1000;
+                break;
+                case "crunchy":
+                    precio += 1500;
+                break;
+                case "sarten":
+                    precio += 2000;
+                break;
+                case "queso":
+                    precio += 2000;
+                break;
+            }
 
             return precio;
         }
 
-        public int getTamanoPrice(string masa)
+        public int getTamanoPrice(string tamanio)
         {
             int precio = 0;
-            if (masa.Equals("pequena"))
-                precio += 4000;
-            if (masa.Equals("mediana"))
-                precio += 4500;
-            if (masa.Equals("grande") )
-                precio += 5000;
-            if (masa.Equals("extra"))
-                precio += 6000;
+            switch (tamanio)
+            {
+                case "pequena":
+                    precio += 4000;
+                    break;
+                case "mediana":
+                    precio += 4500;
+                    break;
+                case "grande":
+                    precio += 5000;
+                    break;
+                case "extra":
+                    precio += 6000;
+                    break;
+            }
+
             return precio;
         }
         public int getIngrePrice(List<string> ingre)
